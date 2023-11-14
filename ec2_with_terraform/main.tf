092388424284f5b0b6590191b60e9da862b52cd4
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-11-14 19:52:16 +0200

    --

---
+++ ec2_with_terraform/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name: "${var.env.prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags = {
+    Name: "${var.env_perfix}-subnet-1"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_perfix}-rtb"
+  }
+}
+
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_perfix}-subnet-1"
+  }
+}
